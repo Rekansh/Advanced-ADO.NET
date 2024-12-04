961b0bc9754b266dd91214fd65235edf4b96b2a8
Rekansh <rekansh.patel@gmail.com>
2024-12-04 10:56:50 +0530

    final stable version checkin

---
+++ ResultSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedADO
+{
+    public class ResultSet
+    {
+        public int ResultIndex { get; set; } = 0;
+        public List<dynamic> ResultData = new List<dynamic>();
+    }
+}
